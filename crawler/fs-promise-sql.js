58cbb3ac718d4b94c08073b4f05e36b019d03e93
Steven <alucky2108@gmail.com>
2021-06-05 21:53:13 +0800

    bluebird+promise+sql

---
+++ crawler/fs-promise-sql.js
@@ -0,0 +1,43 @@
+//引入各個需求
+const axios = require("axios");
+const fs = require("fs/promises");
+const moment = require("moment");
+const mysql = require("mysql");
+const Promise = require("bluebird");
+
+//建立連線所需資料
+let connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "12345",
+  database: "stock",
+});
+//建立連線
+connection = Promise.promisifyAll(connection);
+
+(async function () {
+  try {
+    await connection.connectAsync();
+
+    //讀取STOCK.TXT    老師我改數字TXT怎麼吃不到
+    let stockCode = await fs.readFile("stock.txt", "utf-8");
+    console.log(`我的 stock code: ${stockCode}`);
+    let stock = await connection.queryAsync(`SELECT stock_id FROM stock WHERE stock_id = ${stockCode}`);
+
+    if (stock.length <= 0) {
+        let response = await axios.get(`https://www.twse.com.tw/zh/api/codeQuery?query=${stockCode}`
+    );
+    let answer = response.data.suggestions.shift();
+    let answers = answer.split("\t");
+        // console.log(stockInfos);
+        if (answers.length > 1) {
+            //TODO: answers[0], answers[1]
+            connection.queryAsync(`INSERT INTO stock (stock_id, stock_name) VALUES ('${answers[0]}', '${answers[1]}')`);
+        }
+    }
+} catch (err) {
+    console.error (err);
+} finally {
+    connection.end();
+}
+})();
