0d620862015c7d0701c07382667f0ae26f8e7a52
Steven <alucky2108@gmail.com>
2021-06-08 21:57:34 +0800

    simple web

--- basic/callback.js
+++ basic/callback.js
@@ -1,18 +1,19 @@
 function sum(n, cb) {
-    let result = 0;
-    for (let i = 1; i <= n; i++) {
-      result += i;
-    }
-    // 回頭呼叫
-    cb(result);
+  let result = 0;
+  for (let i = 1; i <= n; i++) {
+    result += i;
   }
-  
-  function reportAns(ans) {
-    console.log(`Hi, 答案是 ${ans}`);
-  }
-  
-  function reportAns2(ans) {
-    console.log(`Hello, 答案是 ${ans}`);
-  }
-  
-  sum(10, reportAns2);
+  // 回頭呼叫
+  cb(result);
+}
+
+function reportAns(ans) {
+  console.log(`Hi, 答案是 ${ans}`);
+}
+
+function reportAns2(ans) {
+  console.log(`Hello, 答案是 ${ans}`);
+}
+
+sum(10, reportAns2);
+
--- crawler/app.js
+++ crawler/app.js
@@ -1,61 +1,87 @@
-const axios = require("axios");
-
-// TODO: 從 stock.txt 讀股票代碼進來
-// filesystem
-// npm i fs ??? -> 不用
-const fs = require("fs");
-
-// fs.readFile("stock.txt", "utf8", (err, data) => {
-//   if (err) {
-//     return console.error("讀檔錯誤", err);
-//   }
-//   console.log(`讀到的 stock code: ${data}`);
-
-function readFilePromise() {
-  return new Promise((resolve, reject) => {
-    fs.readFile("Stock.txt", "utf-8", (err, data) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(data);
-    });
-  });
-}
-
-readFilePromise()
-  .then((result) => {
-    // console.log(result)
-    return axios({
-      method: "get",
-      url: "https://www.twse.com.tw/exchangeReport/STOCK_DAY?",
-      params: {
-        data: 20210529,
-        stockNo: result,
-      },
-    });
-  })
-  .then(function (response) {
-    console.log(response.data.title);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// https://www.twse.com.tw/exchangeReport/STOCK_DAY
+// ?response=json
+// &date=20210523
+// &stockNo=2610
 
 // npm i axios
 // 引入 axios
+const axios = require("axios");
+const moment = require("moment");
+const fs = require("fs/promises");
+const mysql = require("mysql");
+const Promise = require("bluebird");
+require("dotenv").config();
 
-//   axios
-//     .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
-//       params: {
-//         response: "json",
-//         date: "20210523",
-//         stockNo: data,
-//       },
-//     })
-//     .then(function (response) {
-//       if (response.data.stat === "OK") {
-//         console.log(response.data.date);
-//         console.log(response.data.title);
-//       }
-//     });
-// });
+let connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+connection = Promise.promisifyAll(connection);
+
+(async function () {
+  try {
+    await connection.connectAsync();
+
+    let stockCode = await fs.readFile("stock.txt", "utf8");
+    console.log(`讀到的 stock code: ${stockCode}`);
+    let stock = await connection.queryAsync(
+      `SELECT stock_id FROM stock WHERE stock_id = ?`,
+      [stockCode]
+    );
+    console.log("確認資料庫資料筆數:" + stock.length);
+
+    if (stock.length <= 0) {
+      console.log("Start to query name");
+      let response = await axios.get(
+        `https://www.twse.com.tw/zh/api/codeQuery?query=${stockCode}`
+      );
+      let answer = response.data.suggestions.shift();
+      let answers = answer.split("\t");
+      console.log(answers);
+      if (answers.length > 1) {
+        console.log(`儲存股票名稱 ${answers[0]} ${answers[1]}`);
+        console.log("answers:", answers);
+        connection.queryAsync(
+          `INSERT INTO stock (stock_id, stock_name) VALUES (?);`,
+          [answers]
+        );
+      } else {
+        throw "查詢股票名稱錯誤";
+      }
+    }
+
+    // 表示 stock 裡，已經有該 stock id 跟 name 了
+    console.log(`查詢股票成交資料 ${stockCode}`);
+    let prices = await axios.get(
+      "https://www.twse.com.tw/exchangeReport/STOCK_DAY",
+      {
+        params: {
+          response: "json",
+          date: moment().format("YYYYMMDD"),
+          stockNo: stockCode,
+        },
+      }
+    );
+    if (prices.data.stat !== "OK") {
+      throw "查詢股價失敗";
+    }
+    // 處理資料
+    console.log(prices.data.data);
+    // 處理多筆資料
+    // 民國年
+    // '1,639,689,721' 字串、而且有逗號 --> 要處理逗號，然後再轉數字
+    // +13.00 不需要先處理 + - 號
+    // ["日期","成交股數","成交金額","開盤價","最高價","最低價","收盤價","漲跌價差","成交筆數"]
+    // ["110/05/31","184,435,544","3,284,208,706","18.35","18.35","17.50","17.55","-0.55","47,987"]
+
+  } catch (err) {
+    console.error("我是 catch");
+    console.error(err);
+  } finally {
+    connection.end();
+  }
+})();
